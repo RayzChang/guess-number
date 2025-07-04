bc68fe6a45c8017995a41ac3b6f2427213a25522
Rayz Chang <dio19880605@gmail.com>
2025-07-04 16:17:41 +0700

    Add New Fuction

--- import_random.py
+++ import_random.py
@@ -1,13 +1,15 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 
 while True:
+    count += 1
     user = input('請輸入數字:')
     user = int(user)
 
     if user == r:
-        print('猜對了！')
+        print('猜對了！共猜了', count, '次')
         break
     
     elif user > r:
@@ -15,3 +17,5 @@ while True:
 
     elif user < r:
         print('比', user, '大')
+
+    print('這是你猜的第', count, '次')
